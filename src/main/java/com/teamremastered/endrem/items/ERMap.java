668c722fb8e5458b2d5820ca313f7e9d7be37c20
Jack-Bagel <ilysalhi22@gmail.com>
2022-04-06 15:46:34 -0400

    Fixed Structures Location

--- src/main/java/com/teamremastered/endrem/items/ERMap.java
+++ src/main/java/com/teamremastered/endrem/items/ERMap.java
@@ -2,17 +2,26 @@ package com.teamremastered.endrem.items;
 
 import com.teamremastered.endrem.config.ERConfig;
 import com.teamremastered.endrem.registers.RegisterHandler;
+import com.teamremastered.endrem.utils.MultiLocator;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Registry;
+import net.minecraft.data.BuiltinRegistries;
 import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.tags.ConfiguredStructureTags;
+import net.minecraft.tags.TagKey;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.npc.VillagerTrades;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.MapItem;
 import net.minecraft.world.item.trading.MerchantOffer;
+import net.minecraft.world.level.levelgen.feature.ConfiguredStructureFeature;
 import net.minecraft.world.level.saveddata.maps.MapDecoration;
 import net.minecraft.world.level.saveddata.maps.MapItemSavedData;
+import net.minecraft.world.level.storage.loot.functions.ExplorationMapFunction;
+import net.minecraftforge.registries.ForgeRegistries;
 
 import javax.annotation.Nonnull;
 import java.util.Random;
@@ -30,9 +39,10 @@ public class ERMap {
         return Integer.parseInt(ERConfig.MAP_TRADE_VALUES.getList().get(2));
     }
 
+
     public static ItemStack createMap(ServerLevel serverLevel, BlockPos playerPosition) {
         // Get position of marker
-        BlockPos structurePos = RegisterHandler.MAP_ML.getNearestPosition(serverLevel, playerPosition);
+        BlockPos structurePos = serverLevel.findNearestMapFeature(MultiLocator.ENDREM_MAP_LOCATED, playerPosition, 100, false);
 
         // Create map
         ItemStack stack = MapItem.create(serverLevel, structurePos.getX(), structurePos.getZ(), (byte) 2 , true, true);
--- src/main/java/com/teamremastered/endrem/utils/MultiLocator.java
+++ src/main/java/com/teamremastered/endrem/utils/MultiLocator.java
@@ -1,61 +1,16 @@
 package com.teamremastered.endrem.utils;
 
-import com.teamremastered.endrem.EndRemastered;
-import net.minecraft.core.BlockPos;
 import net.minecraft.core.Registry;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.server.level.ServerLevel;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.level.levelgen.feature.ConfiguredStructureFeature;
-import net.minecraft.world.level.levelgen.feature.StructureFeature;
-import net.minecraftforge.fml.ModList;
-import net.minecraftforge.registries.ForgeRegistries;
 
-import java.util.ArrayList;
-import java.util.function.Supplier;
+public interface MultiLocator {
 
-public class MultiLocator {
-    private final Supplier<ArrayList<String>> STRUCTURE_LIST_SUPPLIER;
+    TagKey<ConfiguredStructureFeature<?, ?>> ENDREM_MAP_LOCATED = create("endrem_map_located");
+    TagKey<ConfiguredStructureFeature<?, ?>> ENDREM_EYES_LOCATED = create("endrem_eyes_located");
 
-    public MultiLocator(Supplier<ArrayList<String>> structureListSupplier) {
-        this.STRUCTURE_LIST_SUPPLIER = structureListSupplier;
-    }
-
-    public ArrayList<String> getStructureList() {
-        return this.STRUCTURE_LIST_SUPPLIER.get();
-    }
-
-    public BlockPos getNearestPosition(ServerLevel serverLevel, BlockPos playerPos) {
-        // Temporary values
-        int shortestStructureDistance = -1;
-        BlockPos nearestStructurePos = null;
-
-        for (String structureID : this.getStructureList()) {
-            // Verify if structure mod is loaded
-            String structureModId = structureID.split(":")[0];
-            String structureIdOnly = structureID.split(":")[1];
-            if (ModList.get().isLoaded(structureModId)) {
-
-                // Check if structure id is valid
-                ResourceLocation structureResourceLocation = new ResourceLocation(structureID);
-                if (ForgeRegistries.STRUCTURE_FEATURES.containsKey(structureResourceLocation)) {
-                    // Get distance from player
-                    TagKey<ConfiguredStructureFeature<?, ?>> structureFeature = TagKey.create(Registry.CONFIGURED_STRUCTURE_FEATURE_REGISTRY, new ResourceLocation(structureModId, structureIdOnly));
-                    assert structureFeature != null;
-//                    BlockPos structurePos = serverLevel.getChunkSource().getGenerator().findNearestMapFeature(serverLevel,  structureFeature, playerPos, 100, false);
-                    BlockPos structurePos = serverLevel.findNearestMapFeature(structureFeature, playerPos, 100, false);
-                    // Compare distance to previous minimum
-                    if (structurePos != null) {
-                        int structureDistance = ERUtils.getBlockDistance(structurePos, playerPos);
-                        // if distance is smaller or default value is unchanged, set as new minimum
-                        if (shortestStructureDistance > structureDistance || shortestStructureDistance == -1) {
-                            shortestStructureDistance = structureDistance;
-                            nearestStructurePos = structurePos;
-                        }
-                    }
-                }
-            }
-        }
-        return nearestStructurePos;
+    private static TagKey<ConfiguredStructureFeature<?, ?>> create(String id) {
+        return TagKey.create(Registry.CONFIGURED_STRUCTURE_FEATURE_REGISTRY, new ResourceLocation(id));
     }
 }
